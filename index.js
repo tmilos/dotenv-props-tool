b4d607da75fe10f3bdf4b7635e9e78138e6a1a1f
Milos Tomic <tmilos@gmail.com>
2018-05-11 13:47:35 +0200

    Reading values

--- index.js
+++ index.js
@@ -12,28 +12,50 @@ if (args.help || args._.length < 1) {
 function printUsage(err) {
     if (err) console.log(err)
     console.log("")
-    console.log("Usage:")
-    console.log(" $ dotenv-props-tool command source destination from to [from to ...] [--save]")
+    console.log("Mapping usage:")
+    console.log("  $ dotenv-props-tool command source destination from to [from to ...] [--save]")
     console.log("")
-    console.log("Commands:")
-    console.log("  e2p  Source is dotenv and destination is properties")
-    console.log("  p2e  Source is properties and destination is dotenv")
-    console.log("  p2p  Source is properties and destination is properties")
-    console.log("  e2e  Source is dotenv and destination is dotenv")
+    console.log("  Commands:")
+    console.log("    e2p  Source is dotenv and destination is properties")
+    console.log("    p2e  Source is properties and destination is dotenv")
+    console.log("    p2p  Source is properties and destination is properties")
+    console.log("    e2e  Source is dotenv and destination is dotenv")
     console.log("")
-    console.log("Options:")
-    console.log("  --save       Save output to destination rather then print to stdout")
-    console.log("  --save file  Save output to file")
+    console.log("  Arguments after command come in pairs, first is the key from the source and second is the key in destination")
+    console.log("  If source key does not exist in the source that the key value as provided is written to destination")
+    console.log("  wich might be hanndy in setting fixed string literals to the destination")
+    console.log("")
+    console.log("  Options:")
+    console.log("    --save       Save output to destination rather then print to stdout")
+    console.log("    --save file  Save output to file")
+    console.log("")
+    console.log("Readig usage:")
+    console.log("   $ dotenv-props-tool command key")
+    console.log("")
+    console.log("  Commands:")
+    console.log("    re  Read dotenv")
+    console.log("    rp  Read properties")
     console.log("")
     console.log("Examples:")
     console.log("")
     console.log("  Reads value of MYSQL_USERNAME from the /path/.env and adds that value as spring.datasource.username")
     console.log("  to the /path/file.properties and prints result to stdout:")
-    console.log("    $ dotenv-props-tool e2p /path/.env /path/file.properties MYSQL_USERNAME spring.datasource.username")
+    console.log("")
+    console.log("    $ dotenv-props-tool e2p /path/.env /path/file.properties \\")
+    console.log("        MYSQL_USERNAME spring.datasource.username")
     console.log("")
     console.log("  Reads value of MYSQL_USERNAME from the /path1/.env and adds that value as DB_USER")
     console.log("  to the /path2/.env and saves the result to stdout:")
-    console.log("    $ dotenv-props-tool e2e /path1/.env /path2/.env MYSQL_USERNAME DB_USER --save /path3/.env")
+    console.log("")
+    console.log("    $ dotenv-props-tool e2e /path1/.env /path2/.env \\")
+    console.log("        MYSQL_USERNAME DB_USER \\")
+    console.log("        --save /path3/.env")
+    console.log("")
+    console.log("  Set value of MYSQL_USERNAME to \"john\" in /path/.env:")
+    console.log("")
+    console.log("    $ dotenv-props-tool e2e - /path/.env \\")
+    console.log("        MYSQL_USERNAME john \\")
+    console.log("        --save")
     console.log("")
     process.exit(1)
 }
@@ -71,52 +93,92 @@ function propertiesStringify(obj) {
     return properties.stringify(obj)
 }
 
-var cmdReaderMap = {
-    e2p: [envReader, propertiesReader, propertiesStringify],
-    p2e: [propertiesReader, envReader, propertiesStringify],
-    p2p: [propertiesReader, propertiesReader, propertiesStringify],
-    e2e: [envReader, envReader, propertiesStringify],
+var cmdMap = {
+    help: printUsage,
+    e2p: mapping,
+    p2e: mapping,
+    p2p: mapping,
+    e2e: mapping,
+    re: reading,
+    rp: reading
 }
+
 
 var command = args._.shift()
-if (command.toLowerCase() == 'help') printUsage()
-
-var readers = cmdReaderMap[command]
-if (!readers) {
+if (!cmdMap[command]) {
     printError("Invalid command "+command)
 }
-var sourceReader = readers[0]
-var destinationReader = readers[1]
-var stringify = readers[2]
 
-var sourceFile = args._.shift()
-var destinationFile = args._.shift()
+cmdMap[command]()
 
-sourceReader(sourceFile, function(err, sourceObj) {
-    if (err) printError(err)
 
-    destinationReader(destinationFile, function(err, destinationObj) {
+function mapping() {
+    var cmdReaderMap = {
+        e2p: [envReader, propertiesReader, propertiesStringify],
+        p2e: [propertiesReader, envReader, propertiesStringify],
+        p2p: [propertiesReader, propertiesReader, propertiesStringify],
+        e2e: [envReader, envReader, propertiesStringify],
+    }
+    var readers = cmdReaderMap[command]
+    if (!readers) {
+        printError("Invalid mapping command "+command)
+    }
+    var sourceReader = readers[0]
+    var destinationReader = readers[1]
+    var stringify = readers[2]
+    
+    var sourceFile = args._.shift()
+    var destinationFile = args._.shift()
+    
+    sourceReader(sourceFile, function(err, sourceObj) {
+        if (err) printError(err)
+    
+        destinationReader(destinationFile, function(err, destinationObj) {
+            if (err) printError(err)
+    
+            while (args._.length > 0) {
+                var from = args._.shift()
+                var to  = args._.shift()
+    
+                if (typeof sourceObj[from] != "undefined") {
+                    destinationObj[to] = sourceObj[from]
+                } else {
+                    destinationObj[to] = from
+                }
+            }
+    
+            var string = stringify(destinationObj)
+    
+            if (typeof args.save == "boolean") {
+                fs.writeFileSync(destinationFile, string)
+            } else if (typeof args.save == "string") {
+                fs.writeFileSync(args.save, string)
+            } else {
+                console.log(string)
+            }
+        })
+    })    
+}
+
+function reading() {
+    var readMap = {
+        re: envReader,
+        rp: propertiesReader,
+    }
+
+    var reader = readMap[command]
+    if (!reader) {
+        printError("Invalid reading command "+command)
+    }
+
+    var sourceFile = args._.shift()
+
+    reader(sourceFile, function (err, obj) {
         if (err) printError(err)
 
-        while (args._.length > 0) {
-            var from = args._.shift()
-            var to  = args._.shift()
-
-            if (typeof sourceObj[from] != "undefined") {
-                destinationObj[to] = sourceObj[from]
-            } else {
-                destinationObj[to] = from
-            }
-        }
-
-        var string = stringify(destinationObj)
-
-        if (typeof args.save == "boolean") {
-            fs.writeFileSync(destinationFile, string)
-        } else if (typeof args.save == "string") {
-            fs.writeFileSync(args.save, string)
-        } else {
-            console.log(string)
+        var key = args._.shift()
+        if (obj.hasOwnProperty(key)) {
+            console.log(obj[key])
         }
     })
-})
+}
